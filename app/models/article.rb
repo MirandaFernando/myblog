558f7ce63a034a776e5d21a80643bbd953df9976
fernando <fernandomirandalolzeiro@gmail.com>
2021-08-25 10:31:53 -0300

    create categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    belongs_to :category
     validates :title, presence: true, length: { minimum: 5 }
     validates :body, presence: true, length: { minimum: 10 }
 
