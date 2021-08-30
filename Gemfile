d63066913bfef5b74c352bc3335a4e3223649435
fernando <fernandomirandalolzeiro@gmail.com>
2021-08-30 10:48:23 -0300

    added bullet for optimize queries

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem "letter_opener", "~> 1.7"
+  gem "bullet"
 end
 
 group :test do
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,16 +4,21 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
   
   def index
-    category = Category.find_by_name(params[:category]) if params[:category].present?
+    @categories = Category.sorted
+    category = @categories.select { |c| c.name == params[:category] }[0] if params[:category].present?
 
-    @highlights = Article.filter_by_category(category).desc_order.first(3)
+    @highlights = Article.includes([:category, :user])
+                          .filter_by_category(category)
+                          .desc_order
+                          .first(3)
     highlight_ids = @highlights.pluck(:id).join(',')
     
-    @articles = Article.desc_order
+    @articles = Article.includes([:category, :user])
+                       .desc_order
                        .filter_by_category(category)  
                        .without_highlights(highlight_ids)
                        .page(current_page)
-    @categories = Category.sorted
+
   end
   
   def show
